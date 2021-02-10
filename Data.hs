198abf686fd9b1972df0fa21a15dbf04bc7fe2f3
Aragno-Barros <matyaz182@gmail.com>
2021-02-10 18:58:42 -0300

    sopa de letras v1.0

---
+++ Data.hs
@@ -0,0 +1,27 @@
+module Data (grid, languages) where
+
+grid = [ "JFCEHDNAYERRCBD"
+       , "CZSIBEUAMAGFUNM"
+       , "SSHASKELLJOREBH"
+       , "UTAQWABYTGDSVXY"
+       , "ARRLKJBFGHCHULA"
+       , "AAPHPMNBSHRTYUI"
+       , "QWEASGLREPCVBNN"
+       , "ASDXITRMWYSWLPO"
+       , "ZXCBLIEUJTSOMNJ"
+       , "DFGHJRTYUHBZAXS"
+       , "ERFGHNYUJOLKJHG"
+       , "IUHGEDFGCNIJNYG"
+       ]
+
+languages = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"
+            ]
